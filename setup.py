d643a2845c4a5d584584f829a41d73471299f15d
Jake Mathews <mathewsj2@wit.edu>
2017-03-27 18:31:06 -0400

    Updated

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
 import os
+
 from setuptools import setup
+
 
 # Utility function to read the README file.
 # Used for the long_description.  It's nice, because now 1) we have a top level
 # README file and 2) it's easier to type in the README file than to put a raw
 # string in below ...
-
-
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
 setup(
     name="blackduck_sample_project",
-    version="0.0.2",
+    version="0.0.7",
     author="Black Duck Software",
     author_email="",
     description=("A sample project for using the bdsplugin"),
@@ -21,7 +21,7 @@ setup(
     keywords="sample example blackduck bdsplugin",
     url="https://github.com/blackducksoftware/bdsplugin",
     packages=[],
-    install_requires=["bdsplugin", "Delorean", "pynamodb"],
+    install_requires=["Delorean", "pynamodb"],
     long_description=read('README'),
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
@@ -31,7 +31,3 @@ setup(
     entry_points={"distutils.commands": [
         "bdio = bdsplugin.BlackDuckPlugin:BlackDuckCommand"]}
 )
-
-# from bdsplugin import BlackDuckPlugin
-# pkgs = BlackDuckPlugin.get_setup_dependencies("blackduck_sample_project==0.0.1
-# pkg = pkgs.pop(0)
